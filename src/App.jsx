80795115b7c50ed8e8c5ecca97caa2ed8de3fd08
lailykhayatiwebmorn <lailykh97@gmail.com>
2024-06-05 11:10:28 +0700

    web-profile

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AccountSettings from './components/AccountSettings';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <AccountSettings />
+    </div>
+  );
+};
+
+export default App;
