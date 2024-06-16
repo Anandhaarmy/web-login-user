25a341f6b15cedb3a8c331ecb292656aa7e620d9
lailykhayatiwebmorn <lailykh97@gmail.com>
2024-06-16 12:24:42 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import AccountSettings from './components/AccountSettings';
+import './index.css';
+import PaymentConfirmationForm from './components/PaymentConfirmation';
 
-const App = () => {
+function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <AccountSettings />
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-blue-100 p-4 flex justify-between items-center">
+        <img src="logo.png" className="h-10" alt="logo" />
+        <nav>
+          <a href="#" className="mx-2 text-gray-700">Beranda</a>
+          <a href="#" className="mx-2 text-gray-700">Kost Saya</a>
+          <a href="#" className="mx-2 text-gray-700">Tentang Kami</a>
+          <div className="inline-block ml-4">
+            <img src="profile-pic-url.jpg" alt="profile" className="h-10 w-10 rounded-full" />
+          </div>
+        </nav>
+      </header>
+      <main>
+        <PaymentConfirmationForm />
+        {/* Konten lainnya bisa tetap di sini */}
+      </main>
     </div>
   );
-};
+}
 
 export default App;
